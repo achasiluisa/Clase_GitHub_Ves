b6959564aa8a2065d02930559dfbfd916979e8d5
achasiluisa <achasiluisa@itsqmet.edu.ec>
2026-07-22 17:48:48 -0500

    feature: se agrego dos archivos de javascritp de prueba

---
+++ script.js
@@ -0,0 +1,24 @@
+const nombreCompleto = "Thiago";
+const profesion = "Ingeniero de Software y Docente";
+const pais = "Ecuador";
+
+let anosExperiencia = 4;
+let tecnologiaFavorita = "Spring Boot";
+let disponibleParaProyectos = true;
+
+let proximoProyecto;
+
+console.log(nombreCompleto);
+console.log(profesion);
+console.log(anosExperiencia);
+console.log(proximoProyecto);
+
+console.log(`Me llamo ${nombreCompleto} y trabajo como ${profesion}.`);
+console.log(`Tengo ${anosExperiencia} anios de experiencia con ${tecnologiaFavorita}.`);
+console.log(`Disponible para nuevos proyectos: ${disponibleParaProyectos}`);
+
+anosExperiencia = 5;
+console.log(`Ahora tengo ${anosExperiencia} anios de experiencia.`);
+
+proximoProyecto = "Aplicacion de Realidad Aumentada";
+console.log(`Mi proximo proyecto es: ${proximoProyecto}`);
---
+++ script2.js
@@ -0,0 +1,8 @@
+let variable1 = 1;
+let variable2 = "Santiago";
+
+const pi = 3.1415926;
+
+console.log("Hola Mundo " + variable2);
+
+console.log(`Mi nombre ${variable2}`);
